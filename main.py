7a399fcb8886f4b03b986ab96b3b532e781a6b36
n0raml <61773298+n0raml@users.noreply.github.com>
2022-03-05 22:07:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
 import feedparser
 import re
 import notify
+import requests
+import json
 
+def FCMpush(t,c,u):
+    for k in push_config:
+    if os.getenv(k):
+        v = os.getenv(k)
+        push_config[k] = v
+    """
+    Push Notification API 基于xdroid.net的接口 google play可下载
+    """
+    if not push_config.get("FCM_KEY"):
+        print("FCM 服务的 FCM_KEY 未设置!!\n取消推送")
+        return
+    print("FCM 服务启动")
+    
+    url = 'http://xdroid.net/api/message'
+    data = {"k": {push_config.get("FCM_KEY")},"title": t,"content": c,"u": u}
+    response = requests.post(url, data=json.dumps(data)).json()
+
+    if response.get("StatusCode") == 0:
+        print("FCM 推送成功！")
+    else:
+        print("FCM 推送失败！错误信息如下：\n", response)
+        
 #删除多余html标签和超过2048字节数的字
 def delhtml(t):
     pattern = re.compile(r'<[^>]+>',re.S)
@@ -27,9 +51,9 @@ def GetNewRSS(url):
             #打印文章标题
             print(f.feed.title,post.title)
             #<a 超链接套住标题 /a> 文章发布时间 删除html转义了的文章内容
-            #标题有链接notify.send('<a href="'+post.link+'">'+f.feed.title+' - '+post.title+'</a>\n'+post.published,delhtml(post.description))
+            notify.send('<a href="'+post.link+'">'+f.feed.title+' - '+post.title+'</a>\n'+post.published,delhtml(post.description))
             #标题无链接notify.send(f.feed.title+post.title,delhtml(post.description),post.link)
-            notify.send(f.feed.title+post.title,delhtml(post.description),post.link)
+            FCMpush(f.feed.title+post.title,delhtml(post.description),post.link)
             #写入oldrss记录
             oldrss.writelines([f.feed.title,'  ',post.link,'  ',post.title,'\n'])
         oldrss.close()
