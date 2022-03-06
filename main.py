5cff9d68990367d08e744f129ab2327052931f6b
n0raml <61773298+n0raml@users.noreply.github.com>
2022-03-06 11:29:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ def GetNewRSS(url):
             #notify.send(f.feed.title+post.title, delhtml(post.description), post.link)
 
             #使用fcm方式发送 这个消息带链接只可用这种方式 不带链接用send即可
-            notify.fcm(f.feed.title+'  |  '+post.published, post.title+delhtml(post.description), post.link)
+            #notify.fcm(f.feed.title+'  |  '+post.published, post.title+delhtml(post.description), post.link)
 
 
             #写入oldrss记录
