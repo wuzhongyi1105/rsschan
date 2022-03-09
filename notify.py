6201cf047f39f89039af89ef7d7009757d52da34
n0raml <61773298+n0raml@users.noreply.github.com>
2022-03-09 10:29:36 +0800

    Update notify.py

--- notify.py
+++ notify.py
@@ -104,14 +104,16 @@ def fcm(title: str, content: str, link: str) -> None:
     https://github.com/SimonMarquis/FCM-toolbox
     """
     url = "https://fcm.googleapis.com/fcm/send"
-    link1 = {"title":title,"url":link}
+    link1 = {"title":title+content,"url":link}
     link2 = {"link":link1}
     data = {"to":push_config.get("FCM_KEY"),"time_to_live":60,"priority":"high","data":link2}
     #payload="{\"to\":\""+push_config.get("FCM_KEY")+"\",\"time_to_live\":\"60\",\"priority\":\"high\",\"data\":{\"link\":{\"title\":\""+strtitle+strcontent+"\",\"url\":\""+strlink+"\"}}"
     headers = {'authorization': 'key=AAAASwElybY:APA91bFaTT_zKLcLYqB0soW8PJmFFG7x1F3wiR0MGta9lLsU22uAVa0VD_3zzz-OremJKDEWEf52OD554byamcwAmZldgrQKfwAjjbhZz_5DYT-z1gcflUBFSWVQQ9lSE9KwDBNHULvfVKmQwxa7xNwuPHz-VfdTbw'}
     response = requests.request("POST", url, headers=headers, data = json.dumps(data))
 
-    print(response)
+    print(response.requests.headers)
+    print(response.requests.boby)
+    print(response.requests.text)
     
 def bark(title: str, content: str) -> None:
     """
