086bab1985646de9d2985435bc1b5862eb2f9314
n0raml <61773298+n0raml@users.noreply.github.com>
2022-03-09 11:33:05 +0800

    Update notify.py

--- notify.py
+++ notify.py
@@ -118,8 +118,8 @@ def fcm(title: str, content: str, link: str) -> None:
     link2 = {"text":link1}
     data = {"to":push_config.get("FCM_KEY"),"time_to_live":60,"priority":"high","data":link2}
     headers = {'authorization': 'key=AAAASwElybY:APA91bFaTT_zKLcLYqB0soW8PJmFFG7x1F3wiR0MGta9lLsU22uAVa0VD_3zzz-OremJKDEWEf52OD554byamcwAmZldgrQKfwAjjbhZz_5DYT-z1gcflUBFSWVQQ9lSE9KwDBNHULvfVKmQwxa7xNwuPHz-VfdTbw','Content-Type':'application/json'}
-    response = requests.request("POST", url, headers=headers, data = json.dumps(data))
-
+    #response = requests.request("POST", url, headers=headers, data = json.dumps(data))
+    response = requests.request("POST", url, headers=headers, json=data)
     print(response.headers)
     print(response.text)
     
